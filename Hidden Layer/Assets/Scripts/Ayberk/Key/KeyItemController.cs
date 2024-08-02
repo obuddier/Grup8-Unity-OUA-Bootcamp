7eb98db2ba6e73f82c60f2347404ab283ea11f4a
Bahadır Hamza Nacar <83730093+obuddier@users.noreply.github.com>
2024-08-02 18:30:27 +0300

    checkpoint

--- Hidden Layer/Assets/Scripts/Ayberk/Key/KeyItemController.cs
+++ Hidden Layer/Assets/Scripts/Ayberk/Key/KeyItemController.cs
@@ -24,7 +24,7 @@ namespace KeySystem
         public void ObjectInteraction()
         {
             if (door)
-            {
+            {                
                 doorObject.PlayAnimation();
             }
             
